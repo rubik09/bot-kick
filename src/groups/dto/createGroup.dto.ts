99093ee83232a0119457413eb80b66606e1fb3d4
DenisLokhtin <denis.lokhtin@gmail.com>
2024-03-15 11:58:44 +0600

    fix createGroup.dto.ts

--- src/groups/dto/createGroup.dto.ts
+++ src/groups/dto/createGroup.dto.ts
@@ -2,4 +2,4 @@ import { PickType } from '@nestjs/mapped-types';
 
 import { GroupDto } from './group.dto';
 
-export class CreateGroupDto extends PickType(GroupDto, ['telegramId']) {}
+export class CreateGroupDto extends PickType(GroupDto, ['telegramId', 'groupName']) {}
