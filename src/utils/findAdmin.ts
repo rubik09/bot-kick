205282accce3756a9a1fa1fa12ee14278477883e
DenisLokhtin <denis.lokhtin@gmail.com>
2024-03-22 14:27:20 +0600

    fixes

--- src/utils/findAdmin.ts
+++ src/utils/findAdmin.ts
@@ -1,5 +1,5 @@
 import { ChatMember } from 'node-telegram-bot-api';
 
 export default function findBotAdmin(users: ChatMember[], botId: number): boolean {
-  return users.find((user) => user.user?.id === botId) !== undefined;
+  return !!users.find((user) => user.user?.id === botId);
 }
