5cc1d1ace10626cb949445336082c19de1292dff
DenisLokhtin <denis.lokhtin@gmail.com>
2024-03-25 13:09:50 +0600

    add simple auth

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
 import { CreateGroupDto } from './dto/createGroup.dto';
 import { UpdateGroupDto } from './dto/updateGroup.dto';
 import { Group } from './entity/groups.entity';
 import { GroupsService } from './groups.service';
+import { JwtGuard } from '../auth/jwtAuth.guard';
 
 @Controller('groups')
 export class GroupsController {
@@ -15,23 +27,27 @@ export class GroupsController {
   }
 
   @Get()
+  @UseGuards(JwtGuard)
   async getAllGroups(): Promise<Group[]> {
     return this.groupsService.getAllGroups();
   }
 
   @Post()
+  @UseGuards(JwtGuard)
   async createGroup(@Body() createGroupDto: CreateGroupDto) {
     await this.groupsService.createGroup(createGroupDto);
     throw new HttpException('Группа успешно создана', HttpStatus.CREATED);
   }
 
   @Patch(':id')
+  @UseGuards(JwtGuard)
   async updateGroup(@Param('id') id: Group['id'], @Body() updateGroupDto: UpdateGroupDto) {
     const message = await this.groupsService.updateGroup(id, updateGroupDto);
     throw new HttpException(message, HttpStatus.OK);
   }
 
   @Delete(':id')
+  @UseGuards(JwtGuard)
   async deleteGroup(@Param('id') id: Group['id']) {
     const message = await this.groupsService.deleteGroup(id);
     throw new HttpException(message, HttpStatus.OK);
